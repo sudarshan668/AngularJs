73eaaf9b47af8619af2b91207b22dc2b18b2dcf6
sudarshan sonawane <ssid668@hotmail.com>
2018-06-11 17:55:57 +0530

    ui-develop

--- Jquery/main.js
+++ Jquery/main.js
@@ -1,13 +1,26 @@
 ﻿/// <reference path="Scripts/jquery-3.3.1.intellisense.js" />
 $(document).ready(function ()
 {
+    var code="";
     console.log(" document load successfully");
+   
 
-    $("#Button1").click(function () {
+    console.log("form hide successfully");
+
+    $("#formbtnid").click(function () {
+        $("#one").load("formelement.html");
+       
+    })
+
+    $("#hidebtnid").click(function () {
+        $("#formdiv").remove();
+
+    })
+    $("#hederbtnid").click(function () {
         $("#header").toggle();
-    
+       // $("#formdiv").toggle();
     });
-
+   
 
 });
 
